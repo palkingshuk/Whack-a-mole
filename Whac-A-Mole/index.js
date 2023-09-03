f9c29b6a28c2876e0d24a478262020236006415a
palkingshuk <91213378+palkingshuk@users.noreply.github.com>
2023-09-03 14:35:28 +0530

    Add files via upload

---
+++ Whac-A-Mole/index.js
@@ -0,0 +1,50 @@
+const squares = document.querySelectorAll(".square")
+const timeLeft = document.querySelector("#time-left")
+const scoreDisplay = document.querySelector("#score")
+// const mole = document.querySelector(".mole")
+function startGame() {
+
+    let result = 0;
+    let timerId
+    let hitPosition
+    let countDownTimerId
+    let currentTime = 60
+    function randomSquare() {
+        squares.forEach(square => {
+            square.classList.remove('mole')
+        })
+        let randomPosition = squares[Math.floor(Math.random() * 9)]
+        randomPosition.classList.add('mole')
+
+        hitPosition = randomPosition.id
+    }
+
+    squares.forEach(square => {
+        square.addEventListener('mousedown', () => {
+            if (square.id === hitPosition) {
+                result++
+                scoreDisplay.textContent = result
+                hitPosition = NULL
+            }
+        })
+    })
+
+    function moveMole() {
+        timerId = setInterval(randomSquare, 600);
+    }
+    moveMole()
+
+    function countDown() {
+        currentTime--;
+        timeLeft.textContent = currentTime
+        if (currentTime == -1) {
+            // countDownTimerId = NULL;
+            clearInterval(countDownTimerId)
+            alert("Game Over !! Your Final score is: " + result)
+            clearInterval(timerId)
+            timeLeft.textContent = 0;
+        }
+    }
+
+    countDownTimerId = setInterval(countDown, 1000)
+}
